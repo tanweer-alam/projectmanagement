6bd2402343b4f4b3b5aa02b138e53924c6886e6d
Tanweer Alam <tanweet9955@gmail.com>
2026-05-16 17:42:57 +0530

    added connection string

--- ProjectManagement.Web/Program.cs
+++ ProjectManagement.Web/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
