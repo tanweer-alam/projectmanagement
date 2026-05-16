f196dcd24add991ae91fcb03310ce544ca709b11
Tanweer Alam <tanweet9955@gmail.com>
2026-05-16 16:30:10 +0530

    added base Entity class

---
+++ ProjectManagement.Domain/Common/Entity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManagement.Domain.Common
+{
+    public abstract class Entity<TId> : IEquatable<Entity<TId>> where TId : notnull
+    {
+        public TId Id { get; protected set; } = default!;
+        public DateTime CreatedAt { get; set; }
+
+        protected Entity() { }
+        protected Entity(TId id)
+        {
+            Id = id;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Entity<TId> entity && Id.Equals(entity.Id);
+                   
+        }
+        public bool Equals(Entity<TId>? other)
+        {
+            return other is not null && Id.Equals(other.Id);
+        }
+
+        public static bool operator ==(Entity<TId>? left, Entity<TId>? right)
+        {
+            return Equals(left, right);
+        }
+        public static bool operator !=(Entity<TId>? left, Entity<TId>? right)
+        {
+            return Equals(left, right) == false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
