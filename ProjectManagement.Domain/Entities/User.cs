2c022c9a2f1f6fb91c276e878378bf97b72919f1
Tanweer Alam <tanweet9955@gmail.com>
2026-05-16 16:36:48 +0530

    created User entity

---
+++ ProjectManagement.Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using ProjectManagement.Domain.Common;
+using ProjectManagement.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManagement.Domain.Entities
+{
+    public class User : Entity<Guid>
+    {
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public UserRole Role { get; set; }
+        public User() : base() { }
+        public User(Guid id, string email, string firstName, string lastName, UserRole role) : base(id)
+        {
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+            Role = role;
+        }
+    }
+}
---
+++ ProjectManagement.Domain/Enums/UserRole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManagement.Domain.Enums
+{
+    public enum UserRole
+    {
+        Employee = 0,
+        Admin = 1
+    }
+}
