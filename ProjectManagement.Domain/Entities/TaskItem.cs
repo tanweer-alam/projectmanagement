c2101117b38400358330905001e8d4ebf61c8ae4
Tanweer Alam <tanweet9955@gmail.com>
2026-05-16 16:51:44 +0530

    added TaskItem entity

---
+++ ProjectManagement.Domain/Entities/TaskItem.cs
@@ -0,0 +1,35 @@
+﻿using ProjectManagement.Domain.Common;
+using ProjectManagement.Domain.Enums;
+
+namespace ProjectManagement.Domain.Entities;
+
+public class TaskItem : Entity<Guid>
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public TaskItemStatus Status { get; set; }
+    public DateTime? StartedAt { get; set; }
+    public DateTime? CompletedAt { get; set; }
+
+    //Relationships
+    public Guid ProjectId { get; set; } //FK
+    public Guid AssigneeId { get; set; } //FK
+    public Project Project { get; set; } = null!;
+    public User Assignee { get; set; } = null!;
+
+    public TaskItem() { }
+
+    public TaskItem(
+        Guid id, 
+        string title, 
+        string description, 
+        Guid projectId, 
+        Guid assigneeId) : base(id)
+    {
+        Title = title;
+        Description = description;
+        ProjectId = projectId;
+        AssigneeId = assigneeId;
+    }
+
+}
